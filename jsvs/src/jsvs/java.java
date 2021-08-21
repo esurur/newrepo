ce8d021c268aac1f579651c5a750f70ec6f9a2fb
esurur <emanbsurur14@gmail.com>
2021-08-20 23:51:05 -0400

    hi

---
+++ jsvs/src/jsvs/java.java
@@ -0,0 +1,11 @@
+package jsvs;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("hello world");
+		System.out.println ("hello");
+	}
+
+}
